b846d148712ff1dd93b49a1f469f366f2c36e6e5
RycXEpd <48301941+RycXEpd@users.noreply.github.com>
2020-06-05 08:26:11 +0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,10 +1,10 @@
 var main= require('./encrypt')
 
-console.log(main.encode('teststr',main.TYPE_STR))//普通文本加密测试
-var bin=Str2Bin('test')//二进制转换测试
-console.log(bin,main.encode(bin,main.TYPE_BIN))//二进制文本加密测试
-var hex=Str2Hex('Hextest')
-console.log(hex,main.encode(hex,main.TYPE_HEX))//16进制文本加密测试
-var ret=main.encode('testdecode',main.TYPE_STR)//加密供解密测试
+console.log(main.encode('teststr',main.type.STR))//普通文本加密测试
+var bin=main.tools.Str2Bin('test')//二进制转换测试
+console.log(bin,main.encode(bin,main.type.BIN))//二进制文本加密测试
+var hex=main.tools.Str2Hex('Hextest')
+console.log(hex,main.encode(hex,main.type.HEX))//16进制文本加密测试
+var ret=main.encode('testdecode',main.type.STR)//加密供解密测试
 console.log(ret,main.decode(ret))//解密测试
 console.log(main)//打印结构
